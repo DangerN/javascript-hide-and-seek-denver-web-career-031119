ec0f5855e9c4eefcd82893af0d2eaabd276205fb
DangerN <cdneal091@gmail.com>
2019-04-24 11:49:19 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+
+function getFirstSelector(selector) {
+    return document.querySelector(`${selector}`)
+}
+
+function nestedTarget() {
+  return document.querySelector(".target")
+}
+
+function deepestChild() {
+  const bigNode =  document.querySelector("#grand-node div div div div")
+  // bigNode.
+  return bigNode
+}
+
+function increaseRankBy(n) {
+  const rList = document.querySelectorAll(".ranked-list")
+  for ( let list of rList) {
+    for ( let item of list.querySelectorAll("li") ){
+      item.innerHTML = parseInt(item.innerHTML) + n
+    }
+  }
+}
